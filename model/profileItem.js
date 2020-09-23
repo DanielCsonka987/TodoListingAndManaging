959e12b6f7422a4acc211aef43486e7bed62db39
Daniel Csonka <daniel.csonka987@gmail.com>
2020-09-23 15:43:22 +0200

    basis of model impl.ed

---
+++ model/profileItem.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+
+var ProfileItemSchema = mongoose.Schema({
+  first_name: {type: String, require: true, maxlength: 80},
+  last_name: {type: String, maxlength: 80},
+  age: {type: Number, min:5, max: 120 },
+  occupation: {type: String, maxlength: 50}
+});
+
+ProfileItemSchema.virtual('fullName').get(()=>{
+  return `${this.first_name} ${this.last_name}`;
+});
+
+ProfileItemSchema.virtual('getResourceURL').get((routPath)=>{
+  return `${routPath}/${this._id}`;
+});
+
+module.exports = mongoose.model('profiles', ProfileItemSchema);
---
+++ model/todoItem.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+var TodoItemSchema = Schema({
+
+  task: {type: String, require: true, maxlength: 150},
+  priority: {type: Number, require: true, min: 0, max: 10 },
+  notation: {type: String, maxlength: 150},
+  startingDate: {type: Date, default: Date.now()},
+  lastModfingDate: {type: Date, default: Date.now()},
+  status: {type: String, enum: {'Finished', 'Proceeding'} default: 'Proceeding' },
+  owner: {type: Schema.Types.ObjectId}
+});
+
+TodoItemschema.virtual('setStatus').set((status)=>{
+  this.status = status? 'Finished':'Proceeding';
+  this.lastModfingDate = Date.now();
+});
+
+TodoItemSchema.virtual('setNotation').set((notation)=>{
+  this.notation = notation;
+  this.lastModfingDate = Date.now();
+});
+
+module.exports = mongoose.model('todos', TodoItemSchema);
