5c3094a45364c42cf7fc46a470c194780f0b10ab
Daniel Csonka <daniel.csonka987@gmail.com>
2020-11-22 23:19:09 +0100

    seeder basicly read, functionable

---
+++ model/dbDatasToExampleSite.js
@@ -0,0 +1,70 @@
+module.exports.profileContent = [
+  {
+    first_name: 'John',
+    last_name: 'Doe',
+    username: 'JohnD',
+    password: 'test',
+    age: 31,
+    occupation: 'actor'
+  },
+  {
+    first_name: 'Jane',
+    last_name: 'Doe',
+    username: 'janed',
+    password: 'retest',
+    age: 43
+  },
+  {
+    first_name: 'Andrey',
+    last_name: 'Tolstoj',
+    username: 'tolst01',
+    password: 'warpeace',
+    age: 51,
+    occupation: 'writer'
+  }
+]
+
+module.exports.todoContent = [
+  {
+    task: 'Do the testing of services!',
+    priority: 3
+  },
+  {
+    task: 'Make javascript project model layer',
+    priority: 7,
+    notation: 'The deadhline is this weekend!',
+  },
+  {
+    task: 'Call the partner company - what are the other requirements of software',
+    priority: 3,
+    notation: 'IdealReality ltd. - Healthcare-revisor project',
+  },
+  {
+    task: 'Do the loundry',
+    priority: 4
+  },
+  {
+    task: 'Shopping at the mall',
+    priority: 7,
+    notation: 'Find some food for the next week!',
+  },
+  {
+    task: 'Visit the new clab this weekend',
+    priority: 3,
+    notation: 'StPetersberg Rd. 52.',
+  },
+  {
+    task: 'Finsh the new book',
+    priority: 5
+  },
+  {
+    task: 'Find date for meet the graphic designer!',
+    priority: 7,
+    notation: 'Publisher needs your agree for the book-cover',
+  },
+  {
+    task: 'Revise the last topic content!',
+    priority: 6,
+    notation: 'Publisher needs that this month'
+  }
+]
---
+++ model/dbSeeder.js
@@ -0,0 +1,73 @@
+const mongoose = require('mongoose')
+const dbaccess = require('../config/appConfig.js').db_access;
+const bcrypt = require('bcrypt');
+const encrRound = require('../config/appConfig.js').validation_config.encryption_saltrounds;
+
+let profiles = require('./dbDatasToExampleSite.js').profileContent;
+let todos = require('./dbDatasToExampleSite.js').todoContent;
+const todoRation = todos.length / profiles.length;;
+
+const ProfileSchema = require('./profileItem.js');
+const TodoSchema = require('./todoItem.js');
+const dbOpenTime = 1000;
+
+function saveProfile(profileItem, pIndex){
+  ProfileSchema.create(profileItem, (err, doc)=>{
+    if(err) console.log('Profile persist error: ', err);
+
+    console.log(`User no.${pIndex} is created!`)
+    let tIndexes = [];
+    for(let index = pIndex * todoRation; index < pIndex * todoRation + todoRation; index++){
+      tIndexes.push(index);
+    }
+    tIndexes.forEach((defItemIndex, i) => {
+      saveTodo( todos[defItemIndex] , doc._id);
+      console.log(`Todo no.${defItemIndex} to user ${pIndex} is created!`)
+    });
+  })
+}
+
+function saveTodo(todoItem, ownerId){
+  todoItem.owner = ownerId;
+  TodoSchema.create(todoItem, (err, doc)=>{
+    if(err) console.log('Todo persist error ', err)
+  });
+}
+
+mongoose.connect(dbaccess, { useNewUrlParser: true, useUnifiedTopology: true })
+.then( ()=>{
+
+  mongoose.connection.on('error', err=>{ console.log('MongoDB process error: ' + err) })
+  .once('open', ()=>{ console.log('MongoDB opened to seed!') })
+  .once('close', ()=>{
+     console.log('MongoDB closed!')
+     process.exit();
+   });
+
+  mongoose.connection.collections.profiles.drop(err=>{
+    if(err){
+      if(err.code !== 26)   //NameSpaceNotFound = collection already empty
+        console.log('Profiles collection emptiing failed! ', err)
+      }
+  })
+  mongoose.connection.collections.todos.drop(err=>{
+    if(err){
+      if(err.code !== 26)   //NameSpaceNotFound
+      console.log('Todo collection emptiing failed! ', err)
+    }
+  })
+  profiles.forEach((profItem, i) =>{
+    bcrypt.hash(profItem.password, encrRound,  (err, hash)=>{
+      if(err) console.log('Hashing error ' + err);
+      profItem.password = hash;
+      saveProfile(profItem, i);
+    })
+  })
+  setInterval(()=>{
+    mongoose.connection.close();
+  }, dbOpenTime)
+
+})
+.catch(err=> {
+  console.log('Example site DB seeding failed! ' + err)
+})
