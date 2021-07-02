152cb47dc44034ae9236eebacf096f8559a13785
DanielCsonka987 <43179109+DanielCsonka987@users.noreply.github.com>
2021-07-02 19:17:49 +0200

    second deploy attempt saving

--- model/ProfileModel.js
+++ model/ProfileModel.js
@@ -4,7 +4,7 @@ const view = require('../view/databaseView')
 
 const ProfileItemSchema = new Schema({
   first_name: {type: String, required: true, maxlength: 80},
-  last_name: {type: String, maxlength: 80},
+  last_name: {type: String, maxlength: 80, default: ''},
   username: {type: String, required: true, minlength: 4, maxlength: 70},
   password: {type: String, required: true, minlength: 4, maxlength: 60},
   age: {type: Number, min:5, max: 120, default: 18 },
